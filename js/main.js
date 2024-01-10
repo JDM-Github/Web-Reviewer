46642880d454ca81f345893417e7820bd457c79f
JDMaster <numberonejdmaster@gmail.com>
2024-01-10 11:01:26 +0800

    ADD ACCOUNTING

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 
 let number           = -1;
 let maxNumber        = 99;
+
+let isRandomized     = true;
 let reviewMode       = false;
 let isIdentification = false;
 let isEnumeration    = false;
@@ -18,6 +20,12 @@ numToLetter.set( 0, "A");
 numToLetter.set( 1, "B");
 numToLetter.set( 2, "C");
 numToLetter.set( 3, "D");
+numToLetter.set( 4, "E");
+numToLetter.set( 5, "F");
+numToLetter.set( 6, "G");
+numToLetter.set( 7, "H");
+numToLetter.set( 8, "I");
+numToLetter.set( 9, "J");
 
 let questions = null;
 
@@ -157,12 +165,131 @@ const questionsHCI = [
 	[-1, "Since skeuomorphic interfaces are indeed based on direct manipulation, then all direct manipulation interfaces must need to be skeuomorphic.", false]
 ];
 
+const questionsAccounting = [
+
+	["", "It refer to amounts owed to lenders and suppliers.", "Liabilities||Liability"],
+	["", "This business renders services to customers or clients for a fee.", "Service Business"],
+	["", "This business buy goods or commodities and sell them at a profit.", "Merchandising Business||Trading Business||Merchandising/Trading Business"],
+	["", "This business makes \"finished goods\" from \"raw materials\" or unassembled parts. It \"produces\" the goods that it sells.", "Manufacturing Business"],
+	["", "This is OUTFLOW of economic benefits, in simple term, these are COSTS incurred to produce income/revenue", "Expenses||Expense"],
+	["", "These are PROPERTIES owned by the business.", "Assets||Asset"],
+	["", " It is the RESIDUAL INTEREST in the Assets of the business after deducting all its liabilities.",
+		"Owner's Equity||Owners Equity||Capital||Owner's Equity/Capital||Owners Equity/Capital"],
+
+	["", "____ are debt and equity securities that are purchased with the intent of selling them in the \"near term\" or very soon.", "Trading Security||Trading Securities"],
+	["", "____ are claims against debtors or customers arising from services rendered on account and sale of merchandise on account.", "Account Receivable||Account Receivables"],
+	["", "An identifiable non-monetary asset \"without physical substance\" or have no physical appearance but are expected to provide future economic benefits to the company.",
+		"Intangible Assets||Intangible Asset"],
+	["", "Income Earned but not yet received/collected.", "Accrued Income"],
+	["", "\"Total\" depreciated cost of a depreciable asset. (CONTRA-ASSET)", "Accumalated Depreciation"],
+	["", "Expenses PAID IN ADVANCE by the business (\"Paid but not yet incurred\") (CURRENT ASSET).", "Prepaid Expenses|Prepaid Expense"],
+
+	[-1, ["A business owned by only ONE INDIVIDUAL", "A"],
+		"Sole Proprietorship",
+		"Partnership",
+		"Corporation",
+		"Cooperative"
+	],
+	[-1, ["An association of \"TWO or MORE\" persons who bind themselves to contribute money, property or industry(services) to a common fund, with the intention of dividing the profits among themselves.", "B"],
+		"Sole Proprietorship",
+		"Partnership",
+		"Corporation",
+		"Cooperative"
+	],
+	[-1, ["An artifical being(not natural, like human being) created by operation of LAW, having the rights of SUCCESSION, and the POWERS AND ATTRIBUTES expressly authorized by law or incident to its existence.", "C"],
+		"Sole Proprietorship",
+		"Partnership",
+		"Corporation",
+		"Cooperative"
+	],
+	[-1, ["A legal entity owned and democratically controlled by its members. Members often have a close association with the enterprise as producers or consumers of its products or services, or as its employees.", "D"],
+		"Sole Proprietorship",
+		"Partnership",
+		"Corporation",
+		"Cooperative"
+	],
+	[-1, ["The right Equity/Capital Formula", "A"],
+		"Investment + Revenue - Expenses - Drawings(Withdrawal)",
+		"Investment - Revenue + Expenses - Drawings(Withdrawal)",
+		"Revenue - Investment + Expenses + Drawings(Withdrawal)",
+		"Investment + Revenue + Expenses + Drawings(Withdrawal)"
+	],
+
+	["", "Give 3 types of Business mentioned in REVIEWER",
+		[
+			"Service Business",
+			"Manufacturing Business",
+			"Merchandising Business||Trading Business||Merchandising/Trading Business"
+		]
+	],
+	["", "In DEBIT (DEAL), what is the meaning of DEAL",
+		[
+			"Dividend||Dividends",
+			"Expenses||Expense",
+			"Asset||Assets",
+			"Losses||Loss"
+		]
+	],
+	["", "In CREDIT (GIRLS), what is the meaning of GIRLS",
+		[
+			"Gains||Gain",
+			"Income",
+			"Revenue||Revenues",
+			"Liabilities||Liability",
+			"StockHolder||StockHolders||Owners Equity||Owner's Equity"
+		]
+	],
+
+	["", "Give 5 Example of Current Asset",
+		[
+			"Cash",
+			"Trading Security||Trading Securities",
+			"Account Receivables||Account Receivable",
+			"Notes Receivable||Notes Receivables",
+			"Merchandise Inventory",
+			"Accrued Income",
+			"Advances to Employees",
+			"Prepaid Expenses|Prepaid Expense",
+			"Office/Store Supplies||Office Supplies||Store Supplies||Supplies",
+			"Land",
+			"Building",
+			"Machinery and Equipment||Machinery & Equipment",
+			"Intangible Asset||Intangible Assets"
+		], 5
+	],
+	["", "What are the 2 Classification of Assets",
+		[
+			"Current Assets||Current Asset",
+			"Non-Current Assets||Non-Current Asset||Non Current Assets||Non Current Asset"
+		]
+	],
+
+	[-1, "Liabilities also include amounts received in advance for a future sale or for future service to be performed.", true],
+	[-1, "CAPITAL ACOUNT (or EQUITY ACCOUNT) consists of the following: \na. \"Owner's Capital\"\nb. \"Owner's Drawing\"", true],
+
+	[-1, ["All of this is an elements of Financial Statements, EXCEPT", "F"],
+		"INCOME/REVENUE",
+		"EXPENSE",
+		"ASSETS",
+		"LIABILITIES",
+		"OWNER'S EQUITY/CAPITAL",
+		"ACCOUNTS RECEIVABLES"
+	],
+	[-1, ["All of this is an example of Non-Current Assets, EXCEPT", "C"],
+		"Investment Properties",
+		"Intangible Assets",
+		"Inventory",
+		"Equipment"
+	]
+];
+
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has('var')) {
 	const varValue = urlParams.get('var');
 	if (varValue == 'web') questions = questionsWeb;
 	else if (varValue == 'aoop') questions = questionsAOOP;
 	else if (varValue == 'hci') questions = questionsHCI;
+	else if (varValue == 'acc') questions = questionsAccounting;
 } else {
 	questions = questionsHCI;
 }
@@ -179,6 +306,13 @@ const qA     = document.querySelector(`.selection [selection="A"]`);
 const qB     = document.querySelector(`.selection [selection="B"]`);
 const qC     = document.querySelector(`.selection [selection="C"]`);
 const qD     = document.querySelector(`.selection [selection="D"]`);
+const qE     = document.querySelector(`.selection [selection="E"]`);
+const qF     = document.querySelector(`.selection [selection="F"]`);
+const qG     = document.querySelector(`.selection [selection="G"]`);
+const qH     = document.querySelector(`.selection [selection="H"]`);
+const qI     = document.querySelector(`.selection [selection="I"]`);
+const qJ     = document.querySelector(`.selection [selection="J"]`);
+
 const qT     = document.querySelector(`.topNav .questionDiv`      );
 
 const qTrue  = document.querySelector(`.true-false [selection="True"]`);
@@ -197,7 +331,7 @@ nextDiv.addEventListener('click', () => { next(); });
 const prevDiv = document.querySelector(".topNav .prevDiv")
 prevDiv.addEventListener('click', () => { prev(); });
 
-const arrayChoice    =  [qA, qB, qC, qD];
+const arrayChoice    =  [qA, qB, qC, qD, qE, qF, qG, qH, qI, qJ];
 const arrayTrueFalse = [qTrue, qFalse];
 
 for (let i = 0; i < arrayChoice.length; i++) { arrayChoice[i].addEventListener('click', () => { clickFunc(arrayChoice[i], i); }); }
@@ -234,7 +368,7 @@ function updateQuestionaire() {
 	identiFyDiv.style.display  = "none";
 	enumerateDiv.style.display = "none";
 
-	if (questions[number].length == 6) {
+	if (questions[number].length >= 6) {
 		selectionDiv.style.display = "grid";
 		if ( questions[number][0] >= 0) arrayChoice[questions[number][0]].style.backgroundColor = "#FE017F";
 
@@ -244,24 +378,31 @@ function updateQuestionaire() {
 		qC.textContent = questions[number][4];
 		qD.textContent = questions[number][5];
 
+		if (questions[number].length >= 7 ) { qE.textContent = questions[number][6]; qE.style.display = "inline-block"; } else qE.style.display = "none";
+		if (questions[number].length >= 8 ) { qF.textContent = questions[number][7]; qF.style.display = "inline-block"; } else qF.style.display = "none";
+		if (questions[number].length >= 9 ) { qG.textContent = questions[number][8]; qG.style.display = "inline-block"; } else qG.style.display = "none";
+		if (questions[number].length >= 10) { qH.textContent = questions[number][9]; qH.style.display = "inline-block"; } else qH.style.display = "none";
+		if (questions[number].length >= 11) { qI.textContent = questions[number][10];qI.style.display = "inline-block"; } else qI.style.display = "none";
+		if (questions[number].length >= 12) { qJ.textContent = questions[number][11];qJ.style.display = "inline-block"; } else qJ.style.display = "none";
+
 		if (reviewMode) {
-			if ( questions[number][0] >= 0)
-				arrayChoice[questions[number][0]].style.backgroundColor = "#FF193E";
-			else {
-				qA.style.backgroundColor = "#FF193E";
-				qB.style.backgroundColor = "#FF193E";
-				qC.style.backgroundColor = "#FF193E";
-				qD.style.backgroundColor = "#FF193E";
-			}
+			if  ( questions[number][0] >= 0) arrayChoice[questions[number][0]].style.backgroundColor = "#FF193E";
+			else { arrayChoice.forEach(e => { e.style.backgroundColor = "#FF193E"; }); }
 
 			const answerChar = questions[number][1][1];
 			     if (answerChar == "A") qA.style.backgroundColor = "#77ff43";
 			else if (answerChar == "B") qB.style.backgroundColor = "#77ff43";
 			else if (answerChar == "C") qC.style.backgroundColor = "#77ff43";
 			else if (answerChar == "D") qD.style.backgroundColor = "#77ff43";
+			else if (answerChar == "E") qE.style.backgroundColor = "#77ff43";
+			else if (answerChar == "F") qF.style.backgroundColor = "#77ff43";
+			else if (answerChar == "G") qG.style.backgroundColor = "#77ff43";
+			else if (answerChar == "H") qH.style.backgroundColor = "#77ff43";
+			else if (answerChar == "I") qI.style.backgroundColor = "#77ff43";
+			else if (answerChar == "J") qJ.style.backgroundColor = "#77ff43";
 		}
 
-	} else if (questions[number].length == 3) {
+	} else if (questions[number].length >= 3) {
 
 		if (typeof questions[number][2] == "boolean") {
 			trueFalseDiv.style.display = "grid";
@@ -288,21 +429,30 @@ function updateQuestionaire() {
 				const child = enumerateDiv.children[i];
 				child.value = "";
 				child.readOnly = false;
-				if (i < questions[number][2].length) {
+				if (i < ((questions[number].length == 4) ? questions[number][3] : questions[number][2].length)) {
 					child.style.display = "block";
 					child.style.backgroundColor = "var(--choice-color)";
 				} else child.style.display = "none";
 			}
-			let enumAnswer = questions[number][0].split(", ");
+			let enumAnswer = questions[number][0].split("||");
 			if (reviewMode) {
+				let ansCorr = 0;
 				const newArray = getRightArray(questions[number][2], enumAnswer);
 				for (let i = 0; i < newArray.length; i++) {
 					const child = enumerateDiv.children[i];
-					child.value = newArray[i];
+					child.value = newArray[i].substr((newArray[i].startsWith(" : ") ? 3 : 0));
 					child.readOnly = true;
 
-					if (newArray[i].indexOf(":") != -1) child.style.backgroundColor = "#FF193E";
-					else child.style.backgroundColor = "#77ff43";
+					child.style.display = "block";
+					if (newArray[i].indexOf(":") != -1) {
+						if (((questions[number].length == 4) ? questions[number][3] > ansCorr || questions[number][3] > i : true))
+							child.style.backgroundColor = "#FF193E";
+						else child.style.backgroundColor = "#ffff3E";
+					}
+					else { 
+						ansCorr++;
+						child.style.backgroundColor = "#77ff43";
+					}
 				}
 			}
 			else {
@@ -320,7 +470,7 @@ function updateQuestionaire() {
 			identiFyDiv.value = `${questions[number][0]}`;
 			qQ.textContent = questions[number][1];
 			if (reviewMode) {
-				if (identiFyDiv.value.toLowerCase() == questions[number][2].toLowerCase())
+				if (isInArray(identiFyDiv.value, questions[number][2].split("||")))
 					identiFyDiv.style.backgroundColor = "#77ff43"
 				else {
 					identiFyDiv.style.backgroundColor = "#FF193E"
@@ -338,39 +488,50 @@ function updateQuestionaire() {
 }
 
 function arraysAreEqual(arr1, arr2) {
-	if (arr1.length != arr2.length) return false;
-
 	const sortedArr1 = arr1.slice().sort();
 	const sortedArr2 = arr2.slice().sort();
 
-	for (let i = 0; i < sortedArr1.length; i++)
-		if (sortedArr1[i].toLowerCase() != sortedArr2[i].toLowerCase())
-			return false;
-
+	let isFound;
+	for (let i = 0; i < sortedArr2.length; i++) {
+		isFound = false;
+		for (let j = 0; j < sortedArr1.length; j++) {
+			if (isInArray(sortedArr2[i], sortedArr1[j].split("||"))) {
+				sortedArr1.splice(j, 1);
+  				isFound = true;
+				break;
+			}
+		}
+		if (!isFound) return false;
+	}
 	return true;
 }
 
 function getRightArray(arr1, arr2) {
 
-	const resultAns = []
+	const resultAns = [], leftAns = [];
 	const originalArray = arr1.slice().sort();
-	const answerArray = arr2.slice().sort();
+	const answerArray   = arr2.slice().sort();
 
 	let i, j;
-	let isFound = false;
-	for (i = 0; i < originalArray.length; i++) {
-		isFound = false;
-		for (j = 0; j < answerArray.length; j++) {
-			if (originalArray[i].toLowerCase() == answerArray[j].toLowerCase()) {
-				resultAns.push(answerArray[j]);
-  				answerArray  .splice(j, 1);
-  				isFound = true;
+	for (i = 0; i < answerArray.length; i++) {
+		for (j = 0; j < originalArray.length; j++) {
+			if (isInArray(answerArray[i], originalArray[j].split("||"))) {
+				resultAns.push(answerArray[i]);
+  				originalArray.splice(j, 1);
 				break;
 			}
 		}
-		if (!isFound) resultAns.push(` : ${originalArray[i]}`);
 	}
+	for (j = 0; j < originalArray.length; j++) resultAns.push(` : ${originalArray[j]}`);
 	return resultAns;
+}
+
+function isInArray(e, arr) {
+	const sortedArr = arr.slice().sort();
+	for (let i = 0; i < sortedArr.length; i++)
+		if (sortedArr[i].toLowerCase() == e.toLowerCase())
+			return true;
+	return false;
 }
 
 function finish() {
@@ -380,31 +541,23 @@ function finish() {
 	numOfScoreCor    = 0;
 
 	for (let i = 0; i < questions.length; i++) {
-		if (questions[i].length == 6) {
-			if (numToLetter.get(questions[i][0]) == questions[i][1][1]) {
-				totalScore++;
-				numOfScoreCor++;
-			} else numOfScoreInc++;
+		if (questions[i].length >= 6) {
+			if (numToLetter.get(questions[i][0]) == questions[i][1][1]) { totalScore++; numOfScoreCor++; }
+			else numOfScoreInc++;
 
-		} else if (questions[i].length == 3) {
-
+		} else if (questions[i].length >= 3) {
 			if (typeof questions[i][2] == "boolean") {
-				if (questions[i][0] != -1 && (!questions[i][0]) == questions[i][2]) {
-					totalScore++;
-					numOfScoreCor++;
-				} else numOfScoreInc++;
-
+				if (questions[i][0] != -1 && (!questions[i][0]) == questions[i][2]) { totalScore++; numOfScoreCor++; }
+				else numOfScoreInc++;
 			} else {
 				if (Array.isArray(questions[i][2])) {
-					if (arraysAreEqual(questions[i][2], questions[i][0].split(', '))) {
-						totalScore++;
-						numOfScoreCor++;
-					} else numOfScoreInc++;
+					let answerArray = questions[i][0].split("||");
+					if ( ((questions[i].length == 4) ? answerArray.length == questions[i][3] : answerArray.length == questions[i][2].length) &&
+						arraysAreEqual(questions[i][2], answerArray)) { totalScore++; numOfScoreCor++; }
+					else numOfScoreInc++;
 				}
-				else if (questions[i][0].toLowerCase() == questions[i][2].toLowerCase()) {
-					totalScore++;
-					numOfScoreCor++;
-				} else numOfScoreInc++;
+				else if (isInArray(questions[i][0], questions[i][2].split("||"))) { totalScore++; numOfScoreCor++; }
+				else numOfScoreInc++;
 			}
 		}
 	}
@@ -437,12 +590,10 @@ function retry() {
 	reviewMode           = false;
 
 	for (let i = 0; i < questions.length; i++) {
-		if (questions[i].length == 6)
-			questions[i][0] = -1;
-		else if (questions[i].length == 3)
-			if (typeof questions[i][2] == "boolean")
-				questions[i][0] = -1;
-			else questions[i][0] = "";
+		if (questions[i].length >= 6) questions[i][0] = -1;
+		else if (questions[i].length >= 3)
+			if (typeof questions[i][2] == "boolean") questions[i][0] = -1;
+		else questions[i][0] = "";
 	}
 	start();
 }
@@ -454,11 +605,11 @@ function next() {
 	}
 	else if (!reviewMode && isEnumeration) {
 		const newValue = [];
-		for (let i = 0; i < questions[number][2].length; i++) {
+		for (let i = 0; i < ((questions[number].length == 4) ? questions[number][3] : questions[number][2].length); i++) {
 			const child = enumerateDiv.children[i];
 			newValue.push(child.value);
 		}
-		questions[number][0] = newValue.join(", ");
+		questions[number][0] = newValue.join("||");
 		isEnumeration = false;
 	}
 	if (number + 1 >= questions.length || number + 1 >= maxNumber) {
@@ -478,11 +629,11 @@ function prev() {
 
 	} else if (!reviewMode && isEnumeration) {
 		const newValue = [];
-		for (let i = 0; i < questions[number][2].length; i++) {
+		for (let i = 0; i < ((questions[number].length == 4) ? questions[number][3] : questions[number][2].length); i++) {
 			const child = enumerateDiv.children[i];
 			newValue.push(child.value);
 		}
-		questions[number][0] = newValue.join(", ");
+		questions[number][0] = newValue.join("||");
 		isEnumeration = false;
 	}
 	number--;
@@ -498,7 +649,7 @@ function shuffleArray(array) {
 
 function checkScrollable() {
 	const body = document.body;
-	if (body.scrollHeight + 200 > window.innerHeight) {
+	if (body.scrollHeight + 50 > window.innerHeight) {
 		document.querySelector(".credits").style.position = "relative";
 	} else {
 		document.querySelector(".credits").style.position = "fixed";
@@ -506,40 +657,22 @@ function checkScrollable() {
 }
 
 function start() {
-	shuffleArray(questions);
+	if (isRandomized) shuffleArray(questions);
 	questions.forEach((ques) => {
-		
-		if (ques.length == 6) {
-			let startIndex = 0;
-			const newIndex = Math.floor(Math.random() * 4);
+		if (ques.length >= 6) {
+			const newIndex = Math.floor(Math.random() * (ques.length - 2));
 			const newArray = [];
-			if (ques[1][1] == "A") {
-				newArray.push(ques[3]);
-				newArray.push(ques[4]);
-				newArray.push(ques[5]);
-				startIndex = 0;
-			} else if (ques[1][1] == "B") {
-				newArray.push(ques[2]);
-				newArray.push(ques[4]);
-				newArray.push(ques[5]);
-				startIndex = 1;
-			} else if (ques[1][1] == "C") {
-				newArray.push(ques[2]);
-				newArray.push(ques[3]);
-				newArray.push(ques[5]);
-				startIndex = 2;
-			} else if (ques[1][1] == "D") {
-				newArray.push(ques[2]);
-				newArray.push(ques[3]);
-				newArray.push(ques[4]);
-				startIndex = 3;
-			}
+
+			let startIndex = ques[1][1].charCodeAt(0) - 65;
+			for (let i = 0; i < ques.length - 2; i++)
+				if (2+i != startIndex+2) newArray.push(ques[2+i]);
+
 			ques[1][1] = numToLetter.get(newIndex);
 			ques[newIndex+2] = ques[startIndex+2];
 
 			shuffleArray(newArray);
 			for (let i = 0; i < newArray.length; i++) {
-				ques[((newIndex+1+i) % 4)+2] = newArray[i];
+				ques[((newIndex+1+i) % (ques.length - 2))+2] = newArray[i];
 			}
 		}
 	});
